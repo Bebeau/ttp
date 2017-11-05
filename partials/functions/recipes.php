62a873f63f2dd950bd4a481fd28fcf0d0ad2f13a
Bebeau <kyle@theinitgroup.com>
2017-11-05 01:28:12 -0500

    change logic for display of rate recipe button

--- partials/functions/recipes.php
+++ partials/functions/recipes.php
@@ -265,7 +265,8 @@ function recipe_rating() {
         echo '<i class="fa fa-star" data-star="5"></i>';
         echo '<p><span>(</span> <span id="starNumber">'.$rating.'</span> stars <span>out of</span> <span id="ratingNumber">'.$totalRatings.'</span> ratings <span>)</span></p>';
     echo '</article>';
-    if(!is_admin()) {
+    $screen = get_current_screen();
+    if ( $screen->parent_base == 'edit' ) {
         echo '<button class="btn-rate btn-modal" data-modal="rating"><i class="fa fa-angle-right"></i> Rate Recipe</button>';
     }
     echo '<input type="hidden" name="recipe_rating[]" id="recipe_rating" value="'.$rating.'" />';
